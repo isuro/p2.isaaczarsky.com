f77dc2e9eeed1cb7b33c372e16c6f76d95b83084
Isaac Zarsky <idudez@gmail.com>
2013-11-07 17:46:47 -0500

    more useful profile and index

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -1,1 +1,7 @@
-<h1>Welcome to <?=APP_NAME?><?php if($user) echo ', '.$user->first_name; ?></h1>
+<div class="jumbotron">
+	<h1>Welcome to <?=APP_NAME?><?php if($user) echo ', '.$user->first_name; ?>!</h1>
+	<?php if(!$user): ?>
+		<h3><?=APP_NAME?> is a dumb Twitter clone.</h3>
+		<br>
+	<?php endif; ?>
+</div>
--- views/v_users_profile.php
+++ views/v_users_profile.php
@@ -1,1 +1,16 @@
 <h1>This is the profile of <?=$user->first_name?></h1>
+<br>
+<h3><?=$user->first_name?>'s posts:</h3>
+<?php foreach($posts as $post): ?>
+
+	<article>
+
+		<p><?=$post['content']?></p>
+
+		<time datetime="<?=Time::display($post['created'],'Y-m-d G:i')?>">
+			<?=Time::display($post['created'])?>
+		</time>
+
+	</article>
+
+<?php endforeach; ?>
