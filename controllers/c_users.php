c6f5804c7a3cc0735d5d3b82f0ddffafe2a6b9cf
Isaac Zarsky <idudez@gmail.com>
2013-11-07 19:41:08 -0500

    now you can tell you've signed up!

--- controllers/c_users.php
+++ controllers/c_users.php
@@ -54,7 +54,7 @@ class users_controller extends base_controller {
 
         # For now, just confirm they've signed up - 
         # You should eventually make a proper View for this
-        echo 'You\'re signed up';
+        Router::redirect("/");
 
     }
 
